839d0f876b0f0dca81889ab4728a559b692e0be9
Zeyi <zwang107@gmail.com>
2017-10-11 19:34:03 -0400

    make an exe file

--- exe.R
+++ exe.R
@@ -1,4 +1,17 @@
 library(dplyr)
 library(data.table)
+library(lubridate)
 source("update_usd.R")
-update_usd(if_update = TRUE)
+
+a <- update_usd(if_update = TRUE)
+a
+
+x <- paste(a$all[,3], a$all[,4]) %>% as.POSIXct()
+y <- a$all[,1]
+
+plot(x, y)
+
+# lo.model <- loess(y ~ as.numeric(x), data)
+# lines(x, lo.model$fitted, col = "blue")
+kern <- ksmooth(x %>% as.numeric(), y, kernel = "normal", bandwidth = 20000)
+lines(kern, col = "blue")
